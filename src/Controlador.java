6c64f38d88717b9caeb23a4a7cd3f430bcc228f6
María José <118678970+tismajo@users.noreply.github.com>
2023-09-10 16:45:51 -0600

    final

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,11 +1,14 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 
 public class Controlador {
     public void creationVerificationCSV(){
@@ -44,11 +47,23 @@ public class Controlador {
             System.out.print("Ingresa la ubicación del evento: ");
             String location = scn.nextLine();
 
-            System.out.print("Ingresa el ID del recinto: ");
-            String enclosureID = scn.nextLine();
+            int enclosureID;
+            do{
+                System.out.print("Ingresa el ID del recinto (Del 1 al 99, escriba 0 si no tiene recinto): ");
+                enclosureID = scn.nextInt();
+                if (enclosureID < 0 || enclosureID > 99) {
+                    System.out.println("El número está fuera del rango.");
+                }
+            } while (enclosureID < 0 || enclosureID > 99);
 
-            System.out.print("Ingresa la capacidad del recinto: ");
-            int enclosureCapacity = scn.nextInt();
+            int enclosureCapacity;
+            do{
+            System.out.print("Ingresa la capacidad del recinto (entre 10 y 150000): ");
+                enclosureCapacity = scn.nextInt();
+                if (enclosureID < 10 || enclosureID > 150000) {
+                    System.out.println("El número está fuera del rango.");
+                }
+            } while (enclosureID < 10 || enclosureID > 150000);
 
             writer.write(countryID + "," + location + "," + enclosureID + "," + enclosureCapacity);
             writer.newLine();
@@ -76,25 +91,41 @@ public class Controlador {
             System.out.print("Ingrese el nombre del artista: ");
             String artistName = scn.nextLine();
         
-            System.out.print("Ingrese el horario: ");
-            int eventTimeTable = scn.nextInt();
-        
+            int eventTimeTable;
+            do{
+                System.out.print("Ingrese el horario (De 6 a 22): ");
+                eventTimeTable = scn.nextInt();
+                if (eventTimeTable < 6 || eventTimeTable > 22) {
+                    System.out.println("El número está fuera del rango.");
+                }
+            } while (eventTimeTable < 6 || eventTimeTable > 22);
             scn.nextLine(); // Limpia el salto de línea pendiente
         
             System.out.print("Ingrese la fecha: ");
             String eventDateStr = scn.nextLine();
-            // Aquí puedes convertir la cadena eventDateStr a un tipo de dato adecuado (por ejemplo, Date o LocalDate) si es necesario.
         
-            System.out.print("Ingrese la duración: ");
-            int eventDuration = scn.nextInt();
+            int eventDuration;
+            do{
+                System.out.print("Ingrese la duración (Entre 1 hora y 12 horas): ");
+                eventDuration = scn.nextInt();
+                if (eventDuration < 1 || eventDuration > 12) {
+                    System.out.println("El número está fuera del rango.");
+                }
+            } while (eventTimeTable < 6 || eventTimeTable > 22);
         
-            System.out.print("Ingrese la cantidad de asistentes: ");
-            int eventAttendeesNumber = scn.nextInt();
-        
-            // Escribir los datos en una nueva línea en el archivo CSV
+            int eventAttendeesNumber;
+            do{
+                System.out.print("Ingrese la cantidad de asistentes (De 10 a 15000): ");
+                eventAttendeesNumber = scn.nextInt();
+                
+                if (eventAttendeesNumber < 10 || eventAttendeesNumber > 15000) {
+                    System.out.println("El número está fuera del rango.");
+                }
+            } while (eventAttendeesNumber < 10 || eventAttendeesNumber > 15000);
+
             writer.write(eventID + "," + countryID + "," + artistName + "," + eventTimeTable + "," +
             eventDateStr + "," + eventDuration + "," + eventAttendeesNumber);
-            writer.newLine(); // Agregar una nueva línea
+            writer.newLine();
         
             System.out.println("Datos agregados al archivo CSV exitosamente.");
         
@@ -103,5 +134,125 @@ public class Controlador {
         }
     }
     
-    
+    public void consultEnclosuresCountryID(String countryID){
+        String csvArchive = "enclosuresCSV.csv";
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvArchive))) {
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(",");
+                if (datos.length >= 1 && datos[0].equals(countryID)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo CSV: " + e.getMessage());
+        }
+    }
+
+    public void consultEnclosureID(String enclosureID){
+        String csvArchive = "enclosuresCSV.csv";
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvArchive))) {
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(",");
+                if (datos.length >= 3 && datos[2].equals(enclosureID)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo CSV: " + e.getMessage());
+        }
+    }
+
+    public void consultEnclosureeventLocation(String enclosureLocation){
+        String csvArchive = "enclosuresCSV.csv";
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvArchive))) {
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(",");
+                if (datos.length >= 2 && datos[1].equals(enclosureLocation)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo CSV: " + e.getMessage());
+        }
+    }
+
+    public void consultEventID(String eventID){
+        String csvArchive = "eventsCSV.csv";
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvArchive))) {
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(",");
+                if (datos.length >= 2 && datos[1].equals(eventID)) {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo CSV: " + e.getMessage());
+        }
+    }
+
+    public void eventWOenclosure(){
+        String archivoCSV = "enclosuresCSV.csv";
+        String linea;
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+                if (datos.length >= 3 && datos[2].equals("0")) {
+                    System.out.println(linea);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo CSV: " + e.getMessage());
+        }
+    }
+
+    public void assignEventEnclosure(String eventID2, String enclosureID2){
+        // Rutas de los archivos CSV
+        String rutaArchivo1 = "eventsCSV.csv";
+        String rutaArchivo2 = "enclosuresCSV.csv";
+        String rutaArchivo3 = "archivo3.csv";
+
+        try {
+            // Leer archivo 1 y buscar dato en la segunda columna
+            BufferedReader reader1 = new BufferedReader(new FileReader(rutaArchivo1));
+            String lineaArchivo1;
+            String filaEncontradaArchivo1 = null;
+            while ((lineaArchivo1 = reader1.readLine()) != null) {
+                String[] datos = lineaArchivo1.split(",");
+                if (datos.length >= 2 && datos[1].equals(eventID2)) {
+                    filaEncontradaArchivo1 = lineaArchivo1;
+                    break;
+                }
+            }
+            reader1.close();
+
+            // Leer archivo 2 y buscar dato en la tercera columna
+            BufferedReader reader2 = new BufferedReader(new FileReader(rutaArchivo2));
+            String lineaArchivo2;
+            String filaEncontradaArchivo2 = null;
+            while ((lineaArchivo2 = reader2.readLine()) != null) {
+                String[] datos = lineaArchivo2.split(",");
+                if (datos.length >= 3 && datos[2].equals(enclosureID2)) {
+                    filaEncontradaArchivo2 = lineaArchivo2;
+                    break;
+                }
+            }
+            reader2.close();
+
+            // Escribir la fila encontrada en el archivo 3
+            if (filaEncontradaArchivo1 != null && filaEncontradaArchivo2 != null) {
+                FileWriter writer3 = new FileWriter(rutaArchivo3);
+                writer3.write(filaEncontradaArchivo1 + "," + filaEncontradaArchivo2);
+                writer3.close();
+                System.out.println("La información se ha guardado en el archivo 3.");
+            } else {
+                System.out.println("No se encontró la información en los archivos.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Vista.java
+++ src/Vista.java
@@ -10,9 +10,9 @@ public class Vista {
 
     public void mostrarMenu() {
         while (true) {
-            System.out.println("M E N Ú");
+            System.out.println("\nM E N Ú");
             System.out.println("Ingresa el número de opción: ");
-            System.out.println("1. Ingresar datos");
+            System.out.println("1. Ingresar datos\n2. Consultar un recinto\n3. Consultar un evento\n4. Eventos sin recinto\n5. Asignar un evento a un recinto");
             int opcion = scn.nextInt();
 
             switch (opcion) {
@@ -26,11 +26,53 @@ public class Vista {
                     c.addEventInformation();
                 }
                 break;
+
                 case 2:
+                    int answ;
+                    System.out.println("¿Por medio de qué quieres consultar el recinto? \n 1. ID del país\n 2. ID del recinto\n 3. Ubicación\n 4. Salir del programa");
+                    answ = scn.nextInt();
+                    scn = new Scanner(System.in);
+                    switch (answ) {
+                        case 1:
+                        System.out.print("Ingresa el ID del país: ");
+                        String countryID = scn.nextLine();
+                        c.consultEnclosuresCountryID(countryID);
+                        break;
+
+                        case 2:
+                        System.out.print("Ingresa el ID del recinto (0 si aún no está asignado): ");
+                        String enclosureID = scn.nextLine();
+                        c.consultEnclosureID(enclosureID);
+                        break;
+
+                        case 3:
+                        System.out.print("Ingresa la ubicación: ");
+                        String eventLocation = scn.nextLine();
+                        c.consultEnclosureeventLocation(eventLocation);
+                        break;
+
+                        default:
+                        System.out.println("Inténtelo de nuevo");
+                        break;
+                    }
                     break;
                 case 3:
+                scn = new Scanner(System.in);
+                System.out.print("Ingresa el ID del evento: ");
+                    String eventID = scn.nextLine();
+                    c.consultEventID(eventID);
                     break;
                 case 4:
+                    c.eventWOenclosure();
+                case 5:
+                scn = new Scanner(System.in);
+                System.out.println("Ingrese el ID del evento");
+                String eventID2 = scn.nextLine();
+                System.out.println("Ingrese el ID del recinto");
+                String enclosureID2 = scn.nextLine();
+                c.assignEventEnclosure(eventID2, enclosureID2); 
+                break;
+                case 10:
                     System.exit(0);
                     break;
                 default:
@@ -39,9 +81,4 @@ public class Vista {
             }
         }
 }
-
-    public void requestAddInfo(){
-
-    }
-
 }
