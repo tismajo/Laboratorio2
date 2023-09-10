d73412edf5e5af22761bcb132db94cd0e26efb6a
María José <118678970+tismajo@users.noreply.github.com>
2023-09-10 12:06:26 -0600

    Main verifica-crea CSV

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
+import java.io.File;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     /**
      * Primer ciclo, Segundo Semestre
      * Programación Orientada a Objetos
      * @author María José Girón Isidro - 23559
-     * @param args Mostrar el menú principal del programa
-     * @throws Exception
+     * 
      */
     public static void main(String[] args) throws Exception {
         Vista v = new Vista();
+        File enclosuresArchive = new File("EnclosuresCSV.csv");
+        File eventsArchive = new File("EventsCSV.csv");
+
+        if (!enclosuresArchive.exists()){
+            try {
+                enclosuresArchive.createNewFile();
+                System.out.println("Archivo de EnclosuresCSV.csv creado");
+            } catch (IOException e) {
+                System.out.println("Error encontrado" + e.getMessage());
+        }
+
+        if (!eventsArchive.exists()){
+            try {
+                eventsArchive.createNewFile();
+                System.out.println("Archivo de EventsCSV.csv creado");
+            } catch (IOException e) {
+                // TODO: handle exception
+                System.out.println("Error encontrado" + e.getMessage());
+            }
+        }
+
         v.mostrarMenu();
     }
+}
 }
